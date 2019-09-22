7acd4258833f3087d1365543b86abb315cb909cd
Maryann <maryann@Maryanns-MacBook-Pro.local>
2019-09-22 18:08:56 +0100

    Finish assignment 1

--- DisposableIncome.java
+++ DisposableIncome.java
@@ -3,58 +3,59 @@
 /*  SELF ASSESSMENT
    1. Did I use appropriate CONSTANTS instead of numbers within the code?
        Mark out of 10: 10  
-       Comment: Used a constant for the tax rate
+       Comment: I used constants for the tax rate, and for the numbers that I used to convert between decimal and percent and the other way around.
+        This allowed me to avoid having any unexplained numbers in my code.
    2. Did I use easy-to-understand, meaningful CONSTANT names?
-       Mark out of 5:  5
-       Comment:
+       Mark out of 5:  4
+       Comment: My constant names described properly and understandably the values they were represnting.
     3. Did I format the CONSTANT names properly (in UPPERCASE)?
        Mark out of 5:  5
-       Comment:
+       Comment: All of my constants were in uppercase with underscores between words.
    4. Did I use easy-to-understand meaningful variable names?
        Mark out of 10:  9
-       Comment:
+       Comment: My variable names were descriptive and easy-to-understand.
    5. Did I format the variable names properly (in lowerCamelCase)?
        Mark out of 10:  10
-       Comment:
+       Comment: My variables followed this format.
    6. Did I indent the code appropriately?
        Mark out of 10:  10
-       Comment:
+       Comment: Code was indented to indicate the level of the line.
    7. Did I read the input correctly from the user using appropriate question(s)?
        Mark out of 10:  10
-       Comment:
+       Comment: I asked the user accurate questions and read the input properly.
    8. Did I compute the disposable income correctly?
        Mark out of 10:  10
-       Comment:
+       Comment: I correctly computed the disposable income based on the equation provided, with post tax income calculated then fees being suggested.
    9. Did I compute the disposable income percentage correctly?
-       Mark out of 10:  9
-       Comment:
+       Mark out of 10:  10
+       Comment: I correctly calculated the disposable income percentage.
    10. Did I output the correct answer in the correct format (as shown in the examples)?
        Mark out of 10:  10
-       Comment:
+       Comment: I output the answer according to the format in the example, with the money having 2 decimal places and the percent to the nearest whole number.
    11. How well did I complete this self-assessment?
-       Mark out of 10:  10
-       Comment:
-   Total Mark out of 100 (Add all the previous marks):  
+       Mark out of 10:  9
+       Comment: I believe I accuraetly assessed my work in this self-assessment.
+   Total Mark out of 100 (Add all the previous marks): 97
 */
 
 import java.util.Scanner;
 
-public class DisposableIncome {
-	
+
+class DisposableIncome {
 	public static void main(String[] args) {
 		final double TAX_RATE = 35;
 		final double TOTAL_PERCENTAGE = 100;
 		final double PERCENTAGE_TO_DECIMAL = 0.01;
-		final double CENT_VALUE_IN_EUROS = .1; 
+		final double CENT_VALUE_IN_EUROS = .1;
 		
 		Scanner input = new Scanner(System.in);
-		System.out.println("How much are you paid each month?");
+		System.out.print("How much are you paid a month before tax? ");
 		double salary = input.nextDouble();
-		System.out.println("How much do you pay for your accomodations each month?");
+		System.out.print("How much do you pay for your accomodations each month? ");
 		double accomodation = input.nextDouble();
-		System.out.println("How much do you pay for transportation each month?");
+		System.out.print("How much do you pay for transportation each month? ");
 		double transportation = input.nextDouble();
-		System.out.println("How much do you pay for food each month?");
+		System.out.print("How much do you pay for food each month? ");
 		double food = input.nextDouble();
 		input.close();
 		
@@ -62,12 +63,11 @@ public class DisposableIncome {
 		double disposableIncome = afterTaxIncome - accomodation - transportation - food;
 		double percentOriginalIncome = disposableIncome / salary;
 		
-		double roundedDisposableIncome = disposableIncome - ( disposableIncome % CENT_VALUE_IN_EUROS );
-		double roundedPercentOriginalIncome = (int)(TOTAL_PERCENTAGE * percentOriginalIncome); 
+		String roundedDisposableIncome = String.format("%.2f", disposableIncome);
+		int roundedPercentOriginalIncome = (int)(TOTAL_PERCENTAGE * percentOriginalIncome); 
 		
 		System.out.println(
-				"Your monthly disposable income is €" + roundedDisposableIncome + ", which is " + roundedPercentOriginalIncome + "% of your salary." );
+		  "Your monthly disposable income is €" + roundedDisposableIncome + ", which is " + roundedPercentOriginalIncome + "% of your salary." );
  		
 	}
-
 }
