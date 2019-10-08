01db8e972cd8b1eba93ef67c4e9be4b6d11811e5
Maryann <maryann@Maryanns-MacBook-Pro.local>
2019-10-08 09:44:44 +0100

    Finish lab 3(?)

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,3 +1,36 @@
+//Maryann Foley
+
+/* SELF ASSESSMENT 
+   1. Did I use appropriate easy-to-understand, meaningful variables and CONSTANTS within the code?  
+       Mark out of 10: 10
+       Comment: My variable and constant names were descriprive and easy to understand.
+   2. Did I format the variable and CONSTANT names appropriate (in lowerCamelCase and UPPERCASE)? 
+       Mark out of 5: 5
+       Comment: All of my variables are formatted correctly.
+   3. Did I generate the computer's choice in each game correctly using a Random number generator? 
+       Mark out of 10: 10
+       Comment: I correctly generated the computer choice to be in the range of the desired values.
+   4. Did I input the user's choice in each game correctly? 
+       Mark out of 10: 10
+       Comment: My program takes in input after providing a description of what is expected of the user.
+   5. Did I correctly compare the choices and update the score appropriately? 
+       Mark out of 20: 20
+       Comment: I correctly decided the winner based on the user and computer moves and used this to update the score.
+   6. Did I inform the user of who won each game (and why) correctly? 
+       Mark out of 10: 10
+       Comment: I informed the user of the winner of each round and the move chosen by the computer that led to this outcome.
+   7. Did I use an appropriate for loop to allow the player to play 5 games?  There should be only one loop. 
+       Mark out of 20:  20
+       Comment: I used a single for loop to play 5 games with the user
+   8. Did I output the final scores correctly after the 5 games were played. 
+       Mark out of 10:  10
+       Comment: I correctly output the final scores at the end of the 5 games by stating the number of games won by each the player and computer.
+   9. How well did I complete this self-assessment? 
+       Mark out of 5: 5
+       Comment: I answered the questions about my work critically.
+   Total Mark out of 100 (Add all the previous marks): 100
+*/
+
 import java.util.Scanner;
 import java.util.Random;
 
@@ -27,14 +60,11 @@ public class RockPaperScissors {
 		Random generator = new Random();
 		
 		for (int gameNumber = 1; gameNumber <= NUMBER_GAMES; gameNumber++) {
-			playerMove = -1;
 
-			while (playerMove > MAX_VALUE || playerMove < MIN_VALUE){
-				System.out.print("Enter 1 (for Rock) or 2 (for Paper) or 3 (for Scissors): ");
-				playerMove = input.nextInt();
-			}
+			System.out.print("Enter "+ ROCK_VALUE+" (for Rock), "+PAPER_VALUE+" (for Paper), or "+SCISSORS_VALUE+" (for Scissors): ");
+			playerMove = input.nextInt();
 
-			computerMove = generator.nextInt(MAX_VALUE)+1;
+			computerMove = generator.nextInt(MAX_VALUE)+MIN_VALUE;
 			
 			if (computerMove > playerMove) 
 			{
