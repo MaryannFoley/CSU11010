3945e72e73fe114e1ad63473d1d174fae6b7577a
Maryann Foley <maryann.v.foley@gmail.com>
2019-11-15 12:15:33 +0000

    Create MortgageCalculator.java

---
+++ MortgageCalculator.java
@@ -0,0 +1,20 @@
+
+public class MortgageCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static double readDoubleFromUser() {
+		
+	}
+	
+	public static double calculateMonthlyRepayment (double principal, double mortgageLength, double apr) {
+		
+	}
+	
+	public static double calculateMonthstoRepayMortgage (double mortgage, double monthlyRepayment, double annualInterest) {
+		
+	}
+}
